85a6755c810b5ca2e62d7779394458ee8f804fdf
pmarko <p.marko09@yahoo.com>
2024-11-29 14:55:40 +0100

    corrections to apiTests added

--- src/test/java/com/github_proxy2/api/GithubProxyApiTest.java
+++ src/test/java/com/github_proxy2/api/GithubProxyApiTest.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github_proxy2.model.dto.GithubRepositoryDto;
 import com.github_proxy2.model.dto.RepoDto;
+import com.github_proxy2.model.entity.Owner;
+import com.github_proxy2.model.entity.Repo;
+import com.github_proxy2.repository.RepoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +21,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureWireMock(port = 8089)
 public class GithubProxyApiTest {
 
@@ -28,107 +32,125 @@ public class GithubProxyApiTest {
     RestTemplate restTemplate;
 
     @Autowired
+    RepoRepository repoRepository;
+
+    @Autowired
     ObjectMapper mapper;
 
     private static final String OWNER = "ABC";
     private static final String REPO_NAME = "1234";
 
-    private static final RepoDto REPO_DTO = new RepoDto("pmarko09/school-supervision",
+    private static final GithubRepositoryDto GITHUB_REPO_DTO = new GithubRepositoryDto(
+            "pmarko09/school-supervision",
             null, "https://github.com/pmarko09/school-supervision.git", 0,
-            LocalDateTime.of(2024, 9, 22, 16, 39, 38));
+            LocalDateTime.of(2024, 9, 22, 16, 39, 38),
+            new Owner("abc"), "1234");
 
     @Test
     void getRepoFromGithubTest_DataCorrect_RepoDtoReturned() throws JsonProcessingException {
         wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo(
-                        "/repositories/" + OWNER + "/" + REPO_NAME))
+                        "/repos/" + OWNER + "/" + REPO_NAME))
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(mapper.writeValueAsString(REPO_DTO))));
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(mapper.writeValueAsString(GITHUB_REPO_DTO))));
 
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8089")
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8083")
                 .path("/repositories/" + OWNER + "/" + REPO_NAME)
                 .toUriString();
 
         ResponseEntity<RepoDto> response = restTemplate.getForEntity(url, RepoDto.class);
-
         assertEquals(HttpStatus.OK, response.getStatusCode());
-
         RepoDto result = response.getBody();
         assertNotNull(result);
-        assertEquals(REPO_DTO.getFullName(), result.getFullName());
-        assertEquals(REPO_DTO.getDescription(), result.getDescription());
-        assertEquals(REPO_DTO.getStars(), result.getStars());
-        assertEquals(REPO_DTO.getCloneUrl(), result.getCloneUrl());
-        assertEquals(REPO_DTO.getCreatedAt(), result.getCreatedAt());
+        assertEquals(GITHUB_REPO_DTO.getFullName(), result.getFullName());
+        assertEquals(GITHUB_REPO_DTO.getDescription(), result.getDescription());
+        assertEquals(GITHUB_REPO_DTO.getStars(), result.getStars());
+        assertEquals(GITHUB_REPO_DTO.getCloneUrl(), result.getCloneUrl());
+        assertEquals(GITHUB_REPO_DTO.getCreatedAt(), result.getCreatedAt());
     }
 
     @Test
     void saveRepoLocallyTest_DataCorrect_RepoDtoReturned() throws JsonProcessingException {
-        wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo(
-                        "/repositories/" + OWNER + "/" + REPO_NAME))
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo(
+                        "/repos/" + OWNER + "/" + REPO_NAME))
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(mapper.writeValueAsString(REPO_DTO))));
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(mapper.writeValueAsString(GITHUB_REPO_DTO))));
 
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8089")
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8083")
                 .path("/repositories/" + OWNER + "/" + REPO_NAME)
                 .toUriString();
 
-        ResponseEntity<RepoDto> response = restTemplate.postForEntity(url, REPO_DTO, RepoDto.class);
-
+        ResponseEntity<RepoDto> response = restTemplate.postForEntity(url, null, RepoDto.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-
         RepoDto result = response.getBody();
         assertNotNull(result);
-        assertEquals(result.getFullName(), REPO_DTO.getFullName());
-        assertEquals(result.getStars(), REPO_DTO.getStars());
-        assertEquals(result.getDescription(), REPO_DTO.getDescription());
-        assertEquals(result.getCreatedAt(), REPO_DTO.getCreatedAt());
-        assertEquals(result.getCloneUrl(), REPO_DTO.getCloneUrl());
+        assertEquals(result.getFullName(), GITHUB_REPO_DTO.getFullName());
+        assertEquals(result.getStars(), GITHUB_REPO_DTO.getStars());
+        assertEquals(result.getDescription(), GITHUB_REPO_DTO.getDescription());
+        assertEquals(result.getCreatedAt(), GITHUB_REPO_DTO.getCreatedAt());
+        assertEquals(result.getCloneUrl(), GITHUB_REPO_DTO.getCloneUrl());
     }
 
     @Test
     void editRepoLocally_DataCorrect_RepoDtoReturned() throws JsonProcessingException {
-        wireMockServer.stubFor(WireMock.put("/repositories/" + OWNER + "/" + REPO_NAME)
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(mapper.writeValueAsString(REPO_DTO))));
+        Repo savedRepo = new Repo(
+                1L,
+                "pmarko09/school-supervision",
+                "description",
+                "https://github.com/pmarko09/school-supervision.git",
+                0,
+                LocalDateTime.of(2024, 9, 22, 16, 39, 38),
+                OWNER,
+                REPO_NAME
+        );
+        repoRepository.save(savedRepo);
 
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8089")
+        RepoDto editedRepo = new RepoDto(
+                "change1",
+                "change2",
+                "change3",
+                997,
+                LocalDateTime.of(2024, 9, 22, 16, 39, 38)
+        );
+
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8083")
                 .path("/repositories/" + OWNER + "/" + REPO_NAME)
                 .toUriString();
 
         ResponseEntity<RepoDto> response = restTemplate.exchange(
                 url,
                 HttpMethod.PUT,
-                new HttpEntity<>(REPO_DTO),
+                new HttpEntity<>(editedRepo),
                 RepoDto.class
         );
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-
         RepoDto result = response.getBody();
         assertNotNull(result);
-        assertEquals(result.getFullName(), REPO_DTO.getFullName());
-        assertEquals(result.getCloneUrl(), REPO_DTO.getCloneUrl());
-        assertEquals(result.getDescription(), REPO_DTO.getDescription());
-        assertEquals(result.getStars(), REPO_DTO.getStars());
-        assertEquals(result.getCreatedAt(), REPO_DTO.getCreatedAt());
+        assertEquals(editedRepo.getFullName(), result.getFullName());
+        assertEquals(editedRepo.getDescription(), result.getDescription());
+        assertEquals(editedRepo.getStars(), result.getStars());
+        assertEquals(editedRepo.getCloneUrl(), result.getCloneUrl());
     }
 
     @Test
     void deleteRepoTest_DataCorrect_NoContentReturned() throws JsonProcessingException {
+        Repo savedRepo = new Repo(
+                1L,
+                "pmarko09/school-supervision",
+                "description",
+                "https://github.com/pmarko09/school-supervision.git",
+                0,
+                LocalDateTime.of(2024, 9, 22, 16, 39, 38),
+                OWNER,
+                REPO_NAME
+        );
+        repoRepository.save(savedRepo);
 
-        wireMockServer.stubFor(WireMock.delete("/repositories/" + OWNER + "/" + REPO_NAME)
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.NO_CONTENT.value())
-                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(mapper.writeValueAsString(REPO_DTO))));
-
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8089")
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8083")
                 .path("/repositories/" + OWNER + "/" + REPO_NAME)
                 .toUriString();
 
@@ -138,8 +160,6 @@ public class GithubProxyApiTest {
                 HttpEntity.EMPTY,
                 Void.class
         );
-
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
-
 }
--- src/test/java/com/github_proxy2/api/RepoControllerApiTest.java
+++ src/test/java/com/github_proxy2/api/RepoControllerApiTest.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github_proxy2.model.dto.RepoDto;
+import com.github_proxy2.model.entity.Repo;
+import com.github_proxy2.repository.RepoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,50 +20,51 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureWireMock(port = 8089)
 public class RepoControllerApiTest {
-
-    @Autowired
-    WireMockServer wireMockServer;
 
     @Autowired
     RestTemplate restTemplate;
 
     @Autowired
-    ObjectMapper mapper;
+    RepoRepository repoRepository;
 
     private static final String OWNER = "ABC";
     private static final String REPO_NAME = "1234";
 
-    private static final RepoDto REPO_DTO = new RepoDto("pmarko09/school-supervision",
-            null, "https://github.com/pmarko09/school-supervision.git", 0,
-            LocalDateTime.of(2024, 9, 22, 16, 39, 38));
-
     @Test
     void getRepoLocally_DataCorrect_RepoDtoReturned() throws JsonProcessingException {
+        Repo savedRepo = new Repo(
+                1L,
+                "pmarko09/school-supervision",
+                "description",
+                "https://github.com/pmarko09/school-supervision.git",
+                0,
+                LocalDateTime.of(2024, 9, 22, 16, 39, 38),
+                OWNER,
+                REPO_NAME
+        );
+        repoRepository.save(savedRepo);
 
-        wireMockServer.stubFor(WireMock.get("/local/repositories/"
-                        + OWNER + "/" + REPO_NAME)
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(mapper.writeValueAsString(REPO_DTO))));
-
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8089")
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8083")
                 .path("/local/repositories/" + OWNER + "/" + REPO_NAME)
                 .toUriString();
 
-        ResponseEntity<RepoDto> response = restTemplate.getForEntity(url, RepoDto.class);
+        ResponseEntity<RepoDto> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                RepoDto.class
+        );
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-
         RepoDto result = response.getBody();
         assertNotNull(result);
-        assertEquals(REPO_DTO.getFullName(), result.getFullName());
-        assertEquals(REPO_DTO.getDescription(), result.getDescription());
-        assertEquals(REPO_DTO.getStars(), result.getStars());
-        assertEquals(REPO_DTO.getCloneUrl(), result.getCloneUrl());
-        assertEquals(REPO_DTO.getCreatedAt(), result.getCreatedAt());
+        assertEquals(savedRepo.getFullName(), result.getFullName());
+        assertEquals(savedRepo.getDescription(), result.getDescription());
+        assertEquals(savedRepo.getCloneUrl(), result.getCloneUrl());
+        assertEquals(savedRepo.getStars(), result.getStars());
+        assertEquals(savedRepo.getCreatedAt(), result.getCreatedAt());
     }
 }
